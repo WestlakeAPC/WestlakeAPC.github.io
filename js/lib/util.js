8e19aa3d5c709a3e290ba48e2b23253be975d379
AnimatorJoe <josephjin7@gmail.com>
2017-06-15 14:39:45 -0500

    Haha, I am so funny

--- js/lib/util.js
+++ js/lib/util.js
@@ -7,7 +7,7 @@ define(["require", "exports"], function (require, exports) {
     var greeting = (function () {
         function greeting() {
         }
-        greeting.something = "Steven is awesome and helps me do gulp!";
+        greeting.something = "Steven is awesome and helps me do gulp! Also subscribe to Animator Joe's YouTube channel!";
         return greeting;
     }());
     exports.greeting = greeting;
